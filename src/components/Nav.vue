7ebd3b9cf0c2344ffd968cdf7c51710d62ce086a
HXL <2521556749@qq.com>
2022-11-20 16:13:36 +0800

    完成 Nav 样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="nav">
-        <router-link to="/home">
-            <Icon name="home"/>
+        <router-link to="/home" class="item">
+            <Icon name="home" />
             主页</router-link>
-        |
-        <router-link to="/bill">
-            <Icon name="bill"/>
+
+        <router-link to="/bill" class="item">
+            <Icon name="bill" />
             账单</router-link>
-        |
-        <router-link to="/form">
-            <Icon name="form"/>
+
+        <router-link to="/form" class="item">
+            <Icon name="form" />
             报表
         </router-link>
     </div>
@@ -30,7 +30,21 @@
 
 <style lang="scss" scoped>
     .nav {
-        border: 1px solid red;
         background: black;
+        display: flex;
+        font-size: 12px;
+        padding-top: 8px ;
+        padding-bottom: 12px;
+        >.item{
+            width: 33.33333%;
+            justify-content: center;
+            align-items: center;
+            display: flex;
+            flex-direction: column;
+            >.icon{
+                width: 32px;
+                height: 32px;
+            }
+        }
     }
 </style>
