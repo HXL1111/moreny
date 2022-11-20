41ceb58ddbabf72983393b33ed7ee64709ea3f4f
HXL <2521556749@qq.com>
2022-11-20 15:53:45 +0800

    创建 Icon 组件 使用 Icon 组件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,31 +1,36 @@
 <template>
     <div class="nav">
-        <router-link to="/home">主页</router-link>
+        <router-link to="/home">
+            <Icon name="home"/>
+            主页</router-link>
         |
-        <router-link to="/bill">账单</router-link>
+        <router-link to="/bill">
+            <Icon name="bill"/>
+            账单</router-link>
         |
         <router-link to="/form">
-            <svg>
-                <use xlink:href="#bill"/>
-            </svg>
-            报表</router-link>
+            <Icon name="form"/>
+            报表
+        </router-link>
     </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
-    import x from '@/assets/icons/right.svg'
+  import Icon from '@/components/Icon.vue';
 
-  console.log(x);
-  @Component
+  @Component({
+    components: {Icon}
+  })
   export default class Nav extends Vue {
 
   }
 </script>
 
 <style lang="scss" scoped>
-    .nav{
+    .nav {
         border: 1px solid red;
+        background: black;
     }
 </style>
