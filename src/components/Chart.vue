aa808ec4d68af789f3254a31c5375528c77c2aa6
HXL <2521556749@qq.com>
2022-12-13 16:38:39 +0800

    加入点击支出或收入 css

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="chart_wrapper">
         <div class="amount">
-            <span >月支出 <span class="expense"><slot name="expense"/></span></span>
-            <span>月收入 <span class="income"><slot name="income"/></span></span>
+            <span @click="expenseToggle"><span class="circle"><span :class="{selected:expenseSelected}"></span></span>月支出 <span
+                    class="expense"><slot name="expense"/></span></span>
+            <span @click="incomeToggle"><span class="circle"><span :class="{selected:incomeSelected}"></span></span>月收入 <span
+                    class="income"><slot
+                    name="income"/></span></span>
         </div>
         <div class="chart" ref="chart"></div>
     </div>
@@ -19,7 +22,19 @@
   })
   export default class Chart extends Vue {
     @Prop() options?: EChartsOption;
-    chart?: ECharts
+    chart?: ECharts;
+    expenseSelected = true;
+    incomeSelected = false;
+
+    expenseToggle(): void {
+      this.expenseSelected = !this.expenseSelected;
+
+    }
+
+    incomeToggle(): void {
+      this.incomeSelected = !this.incomeSelected;
+    }
+
     mounted(): void | Error {
       if (this.options === undefined) {
         return new Error('options 不存在');
@@ -28,9 +43,10 @@
         this.chart.setOption(this.options);
       }
     }
+
     @Watch('options')
-    onOptionsChange(newValue:EChartsOption):void{
-      this.chart?.setOption(newValue)
+    onOptionsChange(newValue: EChartsOption): void {
+      this.chart?.setOption(newValue);
     }
   }
 </script>
@@ -44,6 +60,29 @@
         font-size: 14px;
 
         > span {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            > .circle {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                width: 10px;
+                height: 10px;
+                border: 1px solid white;
+                border-radius: 50%;
+
+                > span {
+                    width: 6px;
+                    height: 6px;
+                    border-radius: 50%;
+                    &.selected {
+                        background: white;
+                    }
+                }
+            }
+
             > .expense {
                 color: #ef5155;
             }
@@ -53,12 +92,14 @@
             }
         }
     }
-   .chart{
-      height: 400px;
-       &_wrapper{
-           background: #252525;
-           margin: 0 16px 16px 16px;
-           border-radius: 10px;
-       }
-   }
+
+    .chart {
+        height: 400px;
+
+        &_wrapper {
+            background: #252525;
+            margin: 0 16px 16px 16px;
+            border-radius: 10px;
+        }
+    }
 </style>
